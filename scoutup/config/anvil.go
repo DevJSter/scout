864bbb9673d02cef3b21f61f6d69f91ef2b28752
Shubham Tiwari <shubht3303@gmail.com>
2025-07-01 12:15:04 +0530

    configs

--- scoutup/config/anvil.go
+++ scoutup/config/anvil.go
@@ -4,7 +4,7 @@ func PrepareDefaultAnvilConfig() *NetworkConfig {
 	return &NetworkConfig{
 		Chains: []*ChainConfig{
 			{
-				Name:       "Genie Q Chain",
+				Name:       "Thane Testnet Chain",
 				RPCUrl:     "http://13.233.251.224:9650/ext/bc/22RWkERgqVKS42gJMR7MUAEqi1vQkaYC2yiqkeggRMaeuju7gN/rpc",
 				FirstBlock: 0,
 				ChainID:    1143689,
